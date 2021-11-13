bb9b774589afaf4ba6118e6d151c51eac22ab7e3
Alyssa <alyssantos0406@gmail.com>
2021-11-13 09:04:24 -0500

    store orders gui

--- src/application/StoreOrdersController.java
+++ src/application/StoreOrdersController.java
@@ -1,18 +1,37 @@
 package application;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
-/**
- * Controller to the Store Orders UI
- * @author Daniel Flts, Alyssa Santos
- */
 public class StoreOrdersController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button cancelOrder;
+
+    @FXML
+    private Button exportOrders;
+
+    @FXML
+    private TextField orderTotal;
+
+    @FXML
+    private ComboBox<Integer> phoneNumList;
+
+    @FXML
+    private ListView<String> storeOrdersList;
+
+    @FXML
+    void cancelOrder(ActionEvent event) {
+
     }
+
+    @FXML
+    void exportOrders(ActionEvent event) {
+
+    }
+
 }
